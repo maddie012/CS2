037e87964181429c0cc7ee14fde92bffa9a1a81c
maddie012 <melias28@gcds.net>
2026-02-05 11:19:09 -0500

    Add files via upload

--- Shakespear_dictionary.py
+++ Shakespear_dictionary.py
@@ -1,18 +1,30 @@
 
-Hamlet_dict = {}
-def shakespeare_Hamlet():
+hamlet_dict = {}
+boring_words= ["the", "of", "i", "a", "before", "at", "his", "to", "him", "me", "which", "did", "by", "more", "than", "mine", "own", "as", "such", "was", "and", "very", "he", "on", "you", "come", "an", "your", "some", "for", "does", "this", "can", "there", "goes", "am", "even", "have", "those", "had", "not", "been", "here", "of", "do", "these", "my", "almose", "them", "still", "is", "though", "our", "any", "us", "with", "that", "it", "are", "so", "or", "they", "about", "lets", "ever", "we", "yet", "thats", "out", "from", "things", "where", "whats", "myself", "cannot", "then", "seem", "in", "should", "like", "her", "would", "itself", "be", "into", "she", "were", "saw", "their", "each", "while", "else", "himself", "keep", "way","every","said" ,"many","makes","since" ,"after","another","could","theres","hes" ,"cause" ,"most" ,"upon" ,"now","much","if" ,"make" ,"give" ,"who","has","say","what","thing","again","but","once","last","when","Thou", "thy", "thee", "therefore", "tis", "ho", "o", "hath", "let", "may", "awhile", "made", "off", "comes", "art", "might", "thus", "doth", "whose", "why", "ist", "hast", "use", "shall", "ourselves", "twere", "further", "must", "thine", "ay", "indeed", "put", "mean", "alas", "aught", "ha", "dost"]
+
+def hamlet():
     with open('Hamlet.txt', 'r') as file:
         for line in file:
             words = line.split()
+            
             for word in words:
                 word = word.lower()
-                if word not in Hamlet_dict:
-                    Hamlet_dict[word] = 1
+                
+                if word not in hamlet_dict:
+                    hamlet_dict[word] = 1
                 else:
-                    Hamlet_dict[word] = Hamlet_dict[word] + 1
-        for key in list(Hamlet_dict.keys()):
-            if (Hamlet_dict[key]) < 10:
-                del Hamlet_dict[key]
-        for x, y in Hamlet_dict.items():
-            print(x, y)
-shakespeare_Hamlet()
+                    hamlet_dict[word] += 1
+        for key in list(hamlet_dict.keys()):
+            if (hamlet_dict[key]) < 10:
+                del hamlet_dict[key]
+            elif key in boring_words:
+                del hamlet_dict[key]
+        print("Most Common words in Hamlet")
+        
+        #for x, y in hamlet_dict.items():
+            #print(x, y)
+        asc = {k: v for k, v in sorted(hamlet_dict.items(), key=lambda item: item[1])}
+        for x,y in asc.items():
+            print(x,y)
+hamlet()
+
