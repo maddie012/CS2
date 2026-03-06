ad1344ade463b2cfb44d23ad972ecbba5a5398b0
maddie012 <melias28@gcds.net>
2026-03-06 13:26:46 -0500

    Add files via upload

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -100,16 +100,33 @@ def computer():
     elif new_list[1] != "o" and new_list[1] != "x" and ((new_list[0]=="o" and new_list[2]=="o") or (new_list[4]=="o" and new_list[7]=="o")):
         new_list[1] = "x"
         computer_moves.append(2)
+    elif new_list[2] != "o" and new_list[2] !="x" and ((new_list[0]=="o" and new_list[1]=="o") or (new_list[5]=="o" and new_list[8]=="o")or (new_list[4]=="o" and new_list[6]=="o")):
+        new_list[2] = "x"
+        computer_moves.append(3)
     elif new_list[3] != "o" and new_list[3] !="x" and ((new_list[4] == "o" and new_list[5]=="o") or (new_list[0]=="o" and new_list[6]=="o")):
         new_list[3] = "x"
         computer_moves.append(4)
+    elif new_list[5]!="o" and new_list[5]!="x" and ((new_list[3]=="o" and new_list[4]=="o") or (new_list[2]=="o" and new_list[8]=="o")):
+        new_list[5] = "x"
+        computer_moves.append(6)
+    elif new_list[6]!="o" and new_list[6]!="x" and ((new_list[0]=="o" and new_list[3]=="o") or(new_list[7]=="o" and new_list[8]=="o")or(new_list[4]=="o"and new_list[2]=="o")):
+        new_list[6]="x"
+        computer_moves.append(7)
+    elif new_list[7]!="o" and new_list[7]!="x" and ((new_list[1]=="o" and new_list[4]=="o") or (new_list[6]=="o"and new_list[8]=="o")):
+        new_list[7]="x"
+        computer_moves.append(8)
+    elif new_list[8]!="o" and new_list[8]!="x" and ((new_list[6]=="o"and new_list[7]=="o") or (new_list[2]=="o" and new_list[5]=="o") or (new_list[0]=="o" and new_list[4]=="o")):
+        new_list[8]="x"
+        computer_moves.append(9)
     else:
-        move = random.randint(0,8)
-        if new_list[move] != "o" and new_list[move] != "x":
-            new_list[move] = "x"
-            computer_moves.append(move+1)
-        else:
-            computer()
+        while True:
+            move = random.randint(0,8)
+            if new_list[move] != "o" and new_list[move] != "x":
+                new_list[move] = "x"
+                computer_moves.append(move+1)
+                break
+            #else:
+                #computer()
 
 def check_win(player_moves, computer_moves):
     if (1 in player_moves and 2 in player_moves and 3 in player_moves):
@@ -181,53 +198,56 @@ def check_tie():
 #print_board()
 
 def main():
-    question = input("play other person or computer p/c ")
-    if question == "p":
-        print("player1 is o and player2 is x")
-        print_board()
-        while True:
-            player()
-            persongame = check_win(player_moves, player2_moves)
-            if persongame == "win":
-                print_board()
-                break
-            end = check_tie()
-            if end == "tie":
-                print_board()
-                break
+    while True:
+        question = input("play other person or computer or quit p/c/q ")
+        if question == "p":
+            print("player1 is o and player2 is x")
             print_board()
-            player2()
+            while True:
+                player()
+                persongame = check_win(player_moves, player2_moves)
+                if persongame == "win":
+                    print_board()
+                    break
+                end = check_tie()
+                if end == "tie":
+                    print_board()
+                    break
+                print_board()
+                player2()
+                print_board()
+                persongame = check_win(player_moves, player2_moves)
+                if persongame == "win":
+                    print_board()
+                    break
+                end = check_tie()
+                if end == "tie":
+                    print_board()
+                    break
+        elif question == "c":
+            print("player is o and computer is x")
             print_board()
-            persongame = check_win(player_moves, player2_moves)
-            if persongame == "win":
+            while True:
+                player()
+                game = check_win(player_moves, computer_moves)
+                if game == "win":
+                    print_board()
+                    break
+                end = check_tie()
+                if end == "tie":
+                    print_board()
+                    break
+                computer()
                 print_board()
-                break
-            end = check_tie()
-            if end == "tie":
-                print_board()
-                break
-    elif question == "c":
-        print("player is o and computer is x")
-        print_board()
-        while True:
-            player()
-            game = check_win(player_moves, computer_moves)
-            if game == "win":
-                print_board()
-                break
-            end = check_tie()
-            if end == "tie":
-                print_board()
-                break
-            computer()
-            print_board()
-            game = check_win(player_moves, computer_moves)
-            if game == "win":
-                break
-            end = check_tie()
-            if end == "tie":
-                break
-    else:
-        print("did not input correctly")
+                game = check_win(player_moves, computer_moves)
+                if game == "win":
+                    break
+                end = check_tie()
+                if end == "tie":
+                    break
+        elif question =="q":
+            exit
+        else:
+            print("did not input correctly")
             
 main()
