99df0038c04d43bc791efa7dda876f844b196df6
maddie012 <melias28@gcds.net>
2026-03-06 11:58:44 -0500

    Add files via upload

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -94,6 +94,12 @@ def computer():
     if new_list[4] != "o" and new_list[4] != "x":
         new_list[4] = "x"
         computer_moves.append(5)
+    elif new_list[4] != "o" and new_list[4] != "x" and ((new_list[3] == "o" and new_list[5] == "o") or (new_list[1]== "o" and new_list[7] == "o") or (new_list[0]=="o" and new_list[8]=="o") or (new_list[2]=="o"and new_list[6] == "o")) :
+        new_list[4] = "x"
+        computer_moves.append(5)
+    elif new_list[3] != "o" and new_list[3] !="x" and ((new_list[4] == "o" and new_list[5]=="o") or (new_list[0]=="o" and new_list[6]=="o")):
+        new_list[3] = "x"
+        computer_moves.append(4)
     else:
         move = random.randint(0,8)
         if new_list[move] != "o" and new_list[move] != "x":
@@ -197,7 +203,6 @@ def main():
             if end == "tie":
                 print_board()
                 break
-
     elif question == "c":
         print("player is o and computer is x")
         print_board()
