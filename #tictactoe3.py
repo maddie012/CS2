ca24b52dfbf6c60548168b2fcca4af134a16f57d
maddie012 <melias28@gcds.net>
2026-04-09 09:55:27 -0400

    Add files via upload

--- #tictactoe3.py
+++ #tictactoe3.py
@@ -1,7 +1,7 @@
 #Author: madeleine elias
 #date: 4/2/26
 #description: user can play tic tac toe against a computer. If user wants to play against another person go to original tic tac toe file called Tic_tac_toe.py
-#sources: old tic tac toe code
+#sources: old tic tac toe code and google
 #log: 3rd tic tac toe code
 
 import random
@@ -35,8 +35,9 @@ callcolumns = [ccolumn1, ccolumn2, ccolumn3]
 cdiag1 = [1,5,9]
 cdiag2 = [3,5,7]
 calldiag = [cdiag1,cdiag2]
+allp = [allrows, allcolumns, alldiag]
+allc = [callrows, callcolumns, calldiag]
 allways = [callrows, callcolumns, calldiag, allrows, allcolumns, alldiag]
-
 
 new_list = [1,2,3,4,5,6,7,8,9]  #this is the real tic tac toe board
 
@@ -55,7 +56,7 @@ def player():
     Arg:none
     Returns:none only edits variable'''
     while True:  #puting the input in a loop and it will only break if you put in a number on the board
-        copy = 0
+        copy = 0  #break the while true loop if the user put correct input
         move = input(("what move do you want to make? "))
         if move == "1" or move == "2" or move == "3" or move == "4" or move == "5" or move == "6" or move == "7" or move == "8" or move == "9":
             copy = copy + 1
@@ -69,19 +70,10 @@ def player():
     player_moves.append(move)
     move = move-1
     new_list[move] = "o"
-    for column in allcolumns:  #this goes through all the player columns/rows/diagonals and removes that move from all of them
-        if (move+1) in column:
-            column.remove(move+1)  #removes the last move from the columns
-            #print(column)
-    for row in allrows:
-        if (move+1) in row:
-            row.remove(move+1)
-            #print(row)
-    for diag in alldiag:
-        if (move+1) in diag:
-            diag.remove(move+1)
-            #print("still player")
-            #print(diag)
+    for ways in allp: #columns, rows, diags
+        for each in ways: #looking at num
+            if(move+1) in each:
+                each.remove(move+1)
 
 def computer():
     '''Description: the computer moves but it depends on where the player has moved beforehand. If the player or computer is about to win it will go there to either block the player or win
@@ -93,29 +85,20 @@ def computer():
             new_list[4] = "x"
             computer_moves.append(5)
             move=5
-            for column in callcolumns: #find what row/column/diagonal the move is in and gets rid of it from there to keep track of what moves have been made
-                if (move) in column:
-                    column.remove(move)  #removes the last move from the columns
-            for row in callrows:
-                if (move) in row:
-                    row.remove(move)
-            for diag in calldiag:
-                if (move) in diag:
-                    diag.remove(move)
-            #print("move 5")
+            for ways in allc:
+                for each in ways:
+                    if (move) in each:
+                        each.remove(move)
             return
         else:
-            #print("in else")
             for ways in allways: #for each list with all rows/columns (allrows)
                 for each in ways: #for each list of columns (row1)
-                    #print(each)
                     if len(each) == 1:
                         if new_list[(each[0]-1)]!="o" and new_list[(each[0]-1)]!="x":
                             new_list[(each[0]-1)] = "x"
                             computer_moves.append(each[0])
                             move = each[0]
                             each.remove(each[0])
-                            #print("checking each")
                             return
             if move == 0:
                 while True:
@@ -123,16 +106,10 @@ def computer():
                     if new_list[move] != "o" and new_list[move] != "x":
                         new_list[move] = "x"
                         computer_moves.append(move+1)
-                        for column in callcolumns:#find what row/column/diagonal the move is in and gets rid of it from there to keep track of what moves have been made
-                            if (move+1) in column:
-                                column.remove(move+1)  
-                        for row in callrows:
-                            if (move+1) in row:
-                                row.remove(move+1)
-                        for diag in calldiag:
-                            if (move+1) in diag:
-                                diag.remove(move+1)
-                        print("random")
+                        for ways in allc:
+                            for each in ways:
+                                if (move+1) in each:
+                                    each.remove(move+1)
                         return
         
 def check_win():
@@ -159,7 +136,6 @@ def check_tie():
         print("tie")
         return("tie")
 
-
 def clearboard():
     '''Description: clears the board by setting everything back to the original so player can play again
     Args: none
@@ -170,27 +146,20 @@ def clearboard():
     row1[:] = [1,2,3]
     row2[:] = [4,5,6]
     row3[:] = [7,8,9]
-    allrows[:] = [row1, row2, row3]
     column1[:] = [1,4,7]
     column2[:] = [2,5,8]
     column3[:] = [3,6,9]
-    allcolumns[:] = [column1, column2, column3]
     diag1[:] = [1,5,9]
     diag2[:] = [3,5,7]
-    alldiag[:] = [diag1,diag2]
     #next ones are just for computer to keep tract they are here for the exact same reason as the others
     crow1[:] = [1,2,3]
     crow2[:] = [4,5,6]
     crow3[:] = [7,8,9]
-    callrows[:] = [crow1, crow2, crow3]
     ccolumn1[:] = [1,4,7]
     ccolumn2[:] = [2,5,8]
     ccolumn3[:] = [3,6,9]
-    callcolumns[:] = [ccolumn1, ccolumn2, ccolumn3]
     cdiag1[:] = [1,5,9]
     cdiag2[:] = [3,5,7]
-    calldiag[:] = [cdiag1,cdiag2]
-    allways[:] = [callrows, callcolumns, calldiag, allrows, allcolumns, alldiag]
     new_list[:] = [1,2,3,4,5,6,7,8,9]  #this is the real tic tac toe board
 
 def main():
@@ -224,5 +193,3 @@ def main():
         else:
             print("you did not input correctly")          
 main()
-
-
