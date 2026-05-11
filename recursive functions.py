67419ad8db2ba7b2102f4e744dfbffff28b59018
maddie012 <melias28@gcds.net>
2026-05-11 09:19:25 -0400

    Add files via upload

--- recursive functions.py
+++ recursive functions.py
@@ -1,64 +1,100 @@
 #recursive functions
+#Author: Madeleine Elias
+#description: has a bunch of recursive functions
+#sources: google(i had to look up how to round, how to use powers, how to find remainders, what eculidian is, what fibonacci is, ect)
 import math
 def factorial(n):
     if n <= 1:
         return 1
-    else:
-        return( n * factorial(n-1))
+    return( n * factorial(n-1))
 def summation(n):
     if n <= 1:
         return 1
-    else:
-        return(n + summation(n-1))
+    return(n + summation(n-1))
 def exponential(a,b,x):
+    '''decriptions: it finds the exponential of a certian value, it uses b and multiples it by a recursive wth x-1 so it will keep multipling until x is 0
+    args:
+        a, b, x:  these are numbers for the exponential function a(b^x)
+    returns:
+        the exponential of the equation'''
     if x==0:
         return a
-    else:
-        return(a*(b**x))
+    return(b*exponential(a,b,x-1))
 def fibonacci(n):
     if n<=2:
         return 1
-    else:
-        return(fibonacci(n-1)+fibonacci(n-2))
+    return(fibonacci(n-1)+fibonacci(n-2))
 def sum_digets(n):
+    '''Description: finds the sum of the digets, first it divides it by ten and finds the remainder(whats in the one place), then it adds that to the sumdigets or n/10 but uses // so it will round to a whole number
+    args: 
+        n: a number that the funcion will get the sum of digits 
+    returns:
+        the sum of the digits of n'''
     if n == 0:
         return 0
-    else:
-        num = math.remainder(n,10)
-        return(num+sum_digets(n//10)) #the num is the remainder of n/10 so it would get the 3, 2 and 1 of 123 but the answer is a bit off because the / does not round
+    num = math.remainder(n,10)
+    return(num+sum_digets(n//10)) #the // rounds it to the nearest whole number
 def product_digets(n):
+    '''Description: finds the product of the digets, first it divides it by ten and finds the remainder(whats in the one place), then it multiples that to the productdigets or n/10 but uses // so it will round to a whole number
+    args: 
+        n: a number that the funcion will get the product of digits 
+    returns:
+        the product of the digits of n'''
     if n <10: #if n is less than ten than it would multiply by 0 and it would be 0
         return n
-    else:
-        num = math.remainder(n,10)
-        return(num*product_digets(n//10))
+    num = math.remainder(n,10)
+    return(num*product_digets(n//10))
 def two_whole(n,x):
+    '''description: finds the product of two whole numbers (does not use recursive)
+    args:
+        n,x: two whole numbers
+    returns:
+        the product of the two numbers'''
     if n==0 or x==0:
         return 0 
-    else:
-        return(x*n)
+    return(x*n)
 def sum_range(s,l):
+    '''description: finds the sum of numbers between s and l
+    args:
+        s: random number smaller than l
+        l: random number larger than s
+    returns:
+        the sum of the numbers between s and l'''
     if s >= (l-1): 
         return 0 
-    else:
-        return((s+1)+sum_range(s+1,l))
+    return((s+1)+sum_range(s+1,l))
 def flip(n):
     if n<10:
         return n
-    else:
-        num = math.remainder(n,10)
-        return(f'{num},{flip(n//10)}')
+    num = math.remainder(n,10)
+    return(f'{num},{flip(n//10)}')
 def eculidean(a,b):
+    '''Description: finds the greatest common divisor between a and b
+    args:
+        a: a number that is bigger than b
+        b: a number that is smaller than a
+    returns:
+        the greatest common divisor between a and b'''
     r = math.remainder(a,b)
     if r==0:
         return b
-    else:
-        return(eculidean(b,r))
+    return(eculidean(b,r))
 def compound_interest(p,r,n,t):
-    if n==0:
-        return
-    else:
-        return(p*((1+(r/n))**(n*t)))
+    '''Description: returns the the amount of money they will have after compound interest if when the user puts in the amount
+        (this function does not use recursive functions)
+    args:
+        p = initial amount
+        r = annual rate(decimal)
+        n = coumpounding frequency (how many times a year its compounded)
+        t = time
+    returns:
+        the amount of money they will have after calculating interest
+    '''
+    if  t==0 or n==0:
+        return p
+    return(round(exponential(p,(1+(r/n)),(n*t)),2))
+        #return(round((p*((1+(r/n))**(n*t))),3)) #rounds to 2 decimal places
+    
         
 
 def main():
@@ -72,9 +108,5 @@ def main():
     print(sum_range(3,8))
     print(flip(123))
     print(eculidean(270,192))
-    #p = initial amount
-    #r = annual rate(decimal)
-    # n = coumpounding frequency (how many times a year its compounded)
-    # t = time
-    print(compound_interest(10000.0,0.25,12.0,7))
+    print(compound_interest(10000,0.25,12,7))
 main()
